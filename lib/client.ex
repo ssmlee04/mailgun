7cadb655ccf2d8c78b6c245b6610ecc0d0feb202
Shih-Min Lee <ssmlee04@gmail.com>
2019-08-26 16:02:26 -0400

    able to subscribe and unsucscribe to list

--- lib/client.ex
+++ lib/client.ex
@@ -129,6 +129,7 @@ defmodule Mailgun.Client do
     attrs = %{ members: Poison.encode!([email]) }
     ctype   = 'application/x-www-form-urlencoded'
     body    = URI.encode_query(attrs)
+    IO.inspect [conf, :post, url("/lists/#{emaillist}/members.json", conf[:domain]), "api", conf[:key], [], ctype, body]
     request(conf, :post, url("/lists/#{emaillist}/members.json", conf[:domain]), "api", conf[:key], [], ctype, body)
   end
   defp do_unsubscribe_email_list(_, conf, emaillist, email) do
